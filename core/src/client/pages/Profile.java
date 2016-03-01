9f1d8e76c7fcd8a1ef9cd2d5ac3f624ee03e599a
Hongyu Wang <hongywang@hotmail.com>
2016-03-01 10:02:35 -0500

    Renamed App to Mainloop, moved out of com.gdx

--- core/src/client/pages/Profile.java
+++ core/src/client/pages/Profile.java
@@ -3,5 +3,19 @@ package client.pages;
 /**
  * Created by Kevin Zheng on 2016-02-19.
  */
-public class Profile {
+public class Profile extends State{
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
 }
