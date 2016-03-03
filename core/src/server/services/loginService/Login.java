b9d95728c0409704d73a954caf3a5c4ef89e114a
MalusGreen <kevin97illu@gmail.com>
2016-03-02 23:36:47 -0500

    Finished Login Implementation.

--- core/src/server/services/loginService/Login.java
+++ core/src/server/services/loginService/Login.java
@@ -1,12 +1,28 @@
 package server.services.loginService;
 
+import server.model.user.User;
+
 /**
  * Created by Kevin Zheng on 2016-03-02.
  */
 public class Login implements LoginInterface{
+    public Login(){
+
+    }
+
+    /**
+     *
+     * @param user  The username.
+     * @param pass  The password.
+     * @return
+     */
+    @Override
+    public boolean validateLogin(String user, String pass) {
+        return false;
+    }
 
     @Override
-    public boolean validateLogin() {
-        return false;
+    public User retrieveUserData(String user) {
+        return null;
     }
 }
--- core/src/server/services/loginService/LoginInterface.java
+++ core/src/server/services/loginService/LoginInterface.java
@@ -1,9 +1,24 @@
 package server.services.loginService;
 
+import server.model.user.User;
+
 /**
  * Created by Kevin Zheng on 2016-03-02.
  */
 public interface LoginInterface {
-    boolean validateLogin();
+    /**Requests authentication from the server for a user and pass combo.
+     *
+     * @param user  The username.
+     * @param pass  The password.
+     * @return      True if the combination was correct.
+     */
+    boolean validateLogin(String user, String pass);
 
+    /**Requests the server for all relevant information on a user based on
+     * a username.
+     *
+     * @param user  The username of the user.
+     * @return      A user class with all relevant information.
+     */
+    User retrieveUserData(String user);
 }
