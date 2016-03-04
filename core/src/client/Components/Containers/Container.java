3a5480dced1bcd8aeaa5b5dcb7b7f23bea3cbb2f
TootyManGrass <jerryx2468@gmail.com>
2016-03-04 01:26:25 -0500

    Added container class.

---
+++ core/src/client/Components/Containers/Container.java
@@ -0,0 +1,35 @@
+package client.Components.Containers;
+
+import client.Components.Component;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import tools.ServiceList;
+
+public abstract class Container extends Component
+{
+    protected ServiceList<Component> components = new ServiceList<>();
+
+    public Container(){
+
+    }
+
+    public Container(SpriteBatch batch, double x, double y, double width, double height){
+        init(batch, x, y, width, height);
+    }
+
+    public void init(SpriteBatch batch, double x, double y, double width, double height){
+        this.batch = batch;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void addComponent(Component component){
+        this.components.add(component);
+    }
+
+    public void clear(){
+        components.clear();
+    }
+
+}
