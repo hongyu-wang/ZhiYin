dc9f4b030c516a081e1bb586e15cb650b6de4b94
TootyManGrass <jerryx2468@gmail.com>
2016-03-13 18:47:09 -0400

    Finished shells for diary pages.

--- core/src/client/pages/musicDiary/Diary2Shell.java
+++ core/src/client/pages/musicDiary/Diary2Shell.java
@@ -19,8 +19,33 @@ public class Diary2Shell extends State {
         add(background);
 
         Label composeLabel = new Label("COMPOSE");
-        composeLabel.setBounds(0, 1217, 750, 117);
+        composeLabel.setBounds(0, 1200, 750, 134);
         add(composeLabel);
+
+        Button titleButton = new Button(this);
+        titleButton.setBounds(0, 1112, 750, 88);
+        titleButton.setExecutable(new TestExecutable("title"));
+        add(titleButton);
+
+        Button contentButton = new Button(this);
+        contentButton.setBounds(0, 553, 750, 555);
+        contentButton.setExecutable(new TestExecutable("content"));
+        add(contentButton);
+
+        Button recordingButton = new Button(this);
+        recordingButton.setBounds(0, 134 + 135*2, 750, 135);
+        recordingButton.setExecutable(new TestExecutable("recording"));
+        add(recordingButton);
+
+        Button pictureButton = new Button(this);
+        pictureButton.setBounds(0, 134 + 135, 750, 135);
+        pictureButton.setExecutable(new TestExecutable("picture"));
+        add(pictureButton);
+
+        Button videoButton = new Button(this);
+        videoButton.setBounds(0, 134, 750, 135);
+        videoButton.setExecutable(new TestExecutable("video"));
+        add(videoButton);
 
         Button discardButton = new Button(this);
         discardButton.setBounds(0 + 1, 0, 375, 117);
