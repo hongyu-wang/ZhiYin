dd6abf26ddef94e133b6f22fc1ac1b4eec65b637
MalusGreen <kevin97illu@gmail.com>
2016-03-22 03:11:35 -0400

    Artist Talke updated.

--- core/src/client/pages/pageInternal/serverClientInteractions/ArtistTalker.java
+++ core/src/client/pages/pageInternal/serverClientInteractions/ArtistTalker.java
@@ -1,9 +1,16 @@
 package client.pages.pageInternal.serverClientInteractions;
 
+import server.model.media.MMusic;
+
+import java.util.List;
+
 /**
  * Created by Kevin Zheng on 2016-03-22.
  */
 public class ArtistTalker extends Talkers {
+
+    public String name;
+    public List<MMusic> musicList;
 
 
     /*------------------------------------------------------------------------*/
