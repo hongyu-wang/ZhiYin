6cf122ced4ef27c6ff2839b95acd8de164caf744
MalusGreen <kevin97illu@gmail.com>
2016-03-22 00:37:06 -0400

    Finished MusicTalker API

--- core/src/client/pages/pageInternal/serverClientInteractions/MusicTalker.java
+++ core/src/client/pages/pageInternal/serverClientInteractions/MusicTalker.java
@@ -1,11 +1,13 @@
 package client.pages.pageInternal.serverClientInteractions;
 
 import com.badlogic.gdx.graphics.Texture;
+import server.model.media.MAudio;
 import server.model.media.MHashtag;
 import server.model.social.MComment;
 import server.model.user.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kevin Zheng on 2016-03-22.
@@ -18,8 +20,9 @@ public class MusicTalker extends Talkers{
 
     public List<MHashtag> mHashtags;
     public List<MComment> mComments;
+    public List<MAudio>   audioComments;
 
-
+    public Map<MComment, String> comments;
 
     /*------------------------------------------------------------------------*/
 
