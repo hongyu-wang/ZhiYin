bb9e759246b1a2c1412e31d8a3779e51e2825001
Hongyu Wang <hongywang@hotmail.com>
2016-03-28 01:02:18 -0400

    New login. Fancy stuff.

---
+++ core/src/client/events/executables/internalChanges/loginExecutable/ExecuteLogin.java
@@ -0,0 +1,17 @@
+package client.events.executables.internalChanges.loginExecutable;
+
+import client.stateInterfaces.Executable;
+
+
+/**
+ *
+ * Created by Hongyu Wang on 3/28/2016.
+ */
+public class ExecuteLogin implements Executable{
+
+
+    @Override
+    public void execute() {
+        System.out.println("eyyy");
+    }
+}
