727f927fa57181f0f55a442d2055b518cdc23ff0
DawnOfHammers <hairuo.sang@gmail.com>
2016-03-28 18:29:37 -0400

    added timeout and one more artist picture

--- core/src/server/webservices/RequestObject.java
+++ core/src/server/webservices/RequestObject.java
@@ -15,9 +15,10 @@ import tools.serverTools.generators.Tags;
  */
 public class RequestObject implements Net.HttpResponseListener {
     private ModelStorage modelStorage = ModelStorageFactory.createModelStorage();
-    private JsonReader reader = new JsonReader();
     private ObjectMapper objectMapper = new ObjectMapper();
     private Object rOjbect;
+    private int runMax = 100000000;
+    private int runTime = 0;
 
     public static RequestObject newInstance(){
         return new RequestObject();
@@ -33,6 +34,7 @@ public class RequestObject implements Net.HttpResponseListener {
         // LibGDX NET CLASS
         Net.HttpRequest httpGet = new Net.HttpRequest(Net.HttpMethods.GET);
         httpGet.setUrl("http://"+ModelStorage.ipAddress+":8081/webservice/getServerModel/" + key);
+        httpGet.setTimeOut(runMax);
         //httpGet.setHeader("Content-Type", "application/json");
         //httpGet.setHeader("X-Parse-Application-Id", app_id);
         //httpGet.setHeader("X-Parse-REST-API-Key", app_key);
@@ -56,7 +58,7 @@ public class RequestObject implements Net.HttpResponseListener {
             rOjbect = objectMapper.readValue(json, Class.forName(className));
             modelStorage.setModelFromServer((ServerModel)rOjbect);
         } catch (Exception e) {
-            System.out.println(e);
+
         }
     }
 
