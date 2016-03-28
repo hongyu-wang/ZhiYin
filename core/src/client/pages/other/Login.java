630f110b2c6bc3ebc670935226ca9b14f29a737e
Hongyu Wang <hongywang@hotmail.com>
2016-03-28 02:33:00 -0400

    i don't get the error Xd

--- core/src/client/pages/other/Login.java
+++ core/src/client/pages/other/Login.java
@@ -107,10 +107,7 @@ public class Login extends State {
     @Override
     public void update(float dt) {
         super.update(dt);
-        if (label != null && delta%10 == 0){
-            label.setText(stuff[delta2%(stuff.length)]);
-            delta2 ++;
-        }
+
         if (checkPull){
             if (delta%100 == 0) {
 
@@ -142,5 +139,10 @@ public class Login extends State {
     @Override
     public void dispose() {
 
+    }
+
+
+    public void setText(String str){
+        label.setText(str);
     }
 }
