598ac4b5565f40e56f838f9f6e8d2cc221ce2084
DawnOfHammers <hairuo.sang@gmail.com>
2016-04-02 20:08:32 -0400

    updated some stuff

--- core/src/tools/serverTools/server/MockServer.java
+++ core/src/tools/serverTools/server/MockServer.java
@@ -4,8 +4,10 @@ import server.model.media.MText;
 import server.model.structureModels.ServerModel;
 import tools.serverTools.databases.VirtualDatabase;
 import tools.serverTools.generators.SerialGenerator;
+import tools.utilities.Utils;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Kevin Zheng on 2016-03-09.
@@ -52,6 +54,16 @@ public class MockServer {
      */
     public void setModel(ServerModel model){
         database.setModel(model);
+    }
+
+    public List<ServerModel> getUpdates(List<Long> alreadyUpdated){
+        List<ServerModel> rList = Utils.newList();
+        for(Long l : database.getUpdatedKeys()){
+            if(!alreadyUpdated.contains(l)){
+                rList.add(database.getModel(l));
+            }
+        }
+        return rList;
     }
 
 }
