55582cfe4a7b552c1fad10a738d0c9e7e45c2ef1
Hongyu Wang <hongywang@hotmail.com>
2016-04-04 15:38:05 -0400

    Bugfixed LabelTextArea.

--- core/src/com/badlogic/gdx/scenes/scene2d/ui/LabelTextArea.java
+++ core/src/com/badlogic/gdx/scenes/scene2d/ui/LabelTextArea.java
@@ -1,8 +1,6 @@
 package com.badlogic.gdx.scenes.scene2d.ui;
 
 import client.tools.TagParser;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
@@ -10,10 +8,8 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.IntArray;
 
 import java.util.*;
-import java.util.List;
 
 
 /**
@@ -23,11 +19,16 @@ import java.util.List;
 public class LabelTextArea extends WorkingTextArea{
     private static Map<String, Float> characterSets;
 
-    private void initCharacterSets(BitmapFont bmf){
+    public static final String RED = "[#FF0000]";
+
+    private String color;
+
+    private static void initCharacterSets(BitmapFont bmf){
         if (characterSets == null){
             characterSets = new Hashtable<>();
             GlyphLayout layout = new GlyphLayout();
-            for (String character : "`1234567890-=~!@#$%^&*()_+qwertyyuiop[]\\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>? ".split("")) {
+            for (String character : ("`1234567890-=~!@#$%^&*()_+qwertyyuiop[]\\QWERTYUIOP{}|asdf" +
+                    "ghjkl;'ASDFGHJKL:\"zxcvbnm,./ZXCVBNM<>? ").split("")) {
 
                 layout.setText(bmf, character);
                 float width = layout.width;// contains the width of the current set text
@@ -39,11 +40,17 @@ public class LabelTextArea extends WorkingTextArea{
 
     }
 
+    public LabelTextArea(String text, Skin skin){
+        this(text, skin, RED);
+    }
 
 
-    public LabelTextArea(String text, Skin skin) {
+    public LabelTextArea(String text, Skin skin, String color) {
         super(text, skin, false);
 
+
+
+        this.color = color;
 
         TextFieldStyle style = getStyle();
 
@@ -76,9 +83,8 @@ public class LabelTextArea extends WorkingTextArea{
 
     @Override
     protected void drawText (Batch batch, BitmapFont font, float x, float y) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         initCharacterSets(font);
+
         Queue<int []> chars = TagParser.checkForTag(displayText+"");
 
         int [] current = null;
@@ -88,7 +94,6 @@ public class LabelTextArea extends WorkingTextArea{
 
         int total = 0;
         float offsetY = 0;
-        boolean resetOnNewLine = false;
         for (int i = firstLineShowing * 2; i < (firstLineShowing + getLinesShowing()) * 2 && i < linesBreak.size; i += 2) {
             int value = -1;
             if (current != null){
@@ -97,14 +102,14 @@ public class LabelTextArea extends WorkingTextArea{
 
             int currentWidth = 0;
             for (int j = linesBreak.items[i]; j < linesBreak.items[i+1];j++) {
-                if (total != value && !resetOnNewLine) {
+                if (total != value) {
                     font.draw(batch, displayText, x + currentWidth, y + offsetY, j, j + 1, 0, Align.left, false);
                     currentWidth += characterSets.get("" + displayText.subSequence(j, j + 1));
                     total++;
                 } else {
                     if (current!= null) {
                         int tagSection = Math.min(linesBreak.items[i + 1], current[1]);
-                        CharSequence tempText = "[#FF0000]"+displayText.subSequence(j, tagSection)+"[#FFFFFF]";
+                        CharSequence tempText = color+displayText.subSequence(j, tagSection)+"[#FFFFFF]";
                         font.draw(batch, tempText, x + currentWidth, y + offsetY, 0, Align.left, false);
 
                         for (String character : displayText.subSequence(j, tagSection).toString().split("")){
@@ -115,7 +120,6 @@ public class LabelTextArea extends WorkingTextArea{
                         }
 
                         j += displayText.subSequence(j, tagSection).length()-1;
-                        resetOnNewLine = current[1] > tagSection;
                         if (!chars.isEmpty()) {
                             current = chars.remove();
                             value = current[0];
@@ -153,5 +157,8 @@ public class LabelTextArea extends WorkingTextArea{
         return new int[] {left, right};
     }
 
-
+    @Override
+    protected boolean isWordCharacter(char c) {
+        return super.isWordCharacter(c) || c == '#';
+    }
 }
