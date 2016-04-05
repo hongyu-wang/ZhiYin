9363073460070a009e9b6ff1a87f4380ad5ec978
Hongyu Wang <hongywang@hotmail.com>
2016-04-05 13:50:52 -0400

    Wrote ExecutePlayMSnapShot

--- core/src/client/events/executables/internalChanges/schmoferMusicExecutable/ExecutePlayMSnapShot.java
+++ core/src/client/events/executables/internalChanges/schmoferMusicExecutable/ExecutePlayMSnapShot.java
@@ -1,7 +1,10 @@
 package client.events.executables.internalChanges.schmoferMusicExecutable;
 
 import client.stateInterfaces.Executable;
+import server.model.media.MAudio;
 import server.model.media.MSnapShot;
+import tools.AudioTools.AudioPlayer;
+import tools.serverTools.databases.LocalDatabase;
 import tools.serverTools.databases.LocalDatabaseFactory;
 
 /**
@@ -9,14 +12,47 @@ import tools.serverTools.databases.LocalDatabaseFactory;
  */
 public class ExecutePlayMSnapShot implements Executable {
     private long snapShot;
+    private LocalDatabase localDatabase;
 
     public ExecutePlayMSnapShot(MSnapShot snapShot) {
+
+        this.localDatabase = LocalDatabaseFactory.createLocalDatabase();
         this.snapShot = snapShot.getKey();
     }
 
     @Override
     public void execute() {
         MSnapShot snapShot = LocalDatabaseFactory.createLocalDatabase().getModel(this.snapShot);
+        if (os == MAC) {
+            MAudio song = localDatabase.getModel(snapShot.getSong());
+            MAudio voice = localDatabase.getModel(snapShot.getMessage());
+            AudioPlayer audioPlayer = AudioPlayer.getInstance();
+
+            if (audioPlayer.getCurrentAudio()!= null && audioPlayer.getCurrentAudio().getKey() == song.getKey()) {
+
+                if (audioPlayer.isPlaying())
+                    audioPlayer.pause();
+                else {
+                    audioPlayer.prepareToPlay();
+                    audioPlayer.play();
+                }
+
+            } else{
+
+                audioPlayer.setSongSnapShot(voice,song,(double)snapShot.getStartTime());
+                audioPlayer.prepareToPlay();
+                audioPlayer.play();
+            }
+
+        } else{
+            System.out.println("Windows Play");
+        }
+
+
+
+
 
     }
+
 }
+
