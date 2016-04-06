d10cfc7b5be98a7ad62d93d26489077fd60c0e81
Hongyu Wang <hongywang@hotmail.com>
2016-04-06 03:20:15 -0400

    SkinSingleton Changes

--- core/src/client/singletons/SkinSingleton.java
+++ core/src/client/singletons/SkinSingleton.java
@@ -1,7 +1,9 @@
 package client.singletons;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 
 /**
  * Created by Hongyu Wang on 3/20/2016.
@@ -16,7 +18,11 @@ public class SkinSingleton {
             String filepath = StateManager.M == 0.5F ? "SkinAssets1" : "SkinAssets2";
 
             ourInstance = new Skin(Gdx.files.internal(filepath + "/uiskin.json"));
-
+            if (StateManager.M > 1) {
+                for (String name : ourInstance.getAll(BitmapFont.class).keys()) {
+                    ourInstance.getFont(name).getData().setScale(StateManager.M, StateManager.M);
+                }
+            }
         }
 
         return ourInstance;
